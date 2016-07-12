e7fb155d00e50b8b635badf290bd9b6d3f0621ef
icarussil <icarus.sil@gmail.com>
2016-07-12 20:01:22 +0900

    dfwefwefwefwefwef

--- app/src/main/java/com/example/administrator/bloodchecker/Fragment2.java
+++ app/src/main/java/com/example/administrator/bloodchecker/Fragment2.java
@@ -20,6 +20,21 @@ import java.util.Locale;
 
 public class Fragment2 extends Fragment {
 
+    // 연/월 텍스트뷰
+    private TextView tvDate;
+
+    // 그리드뷰 어댑터
+    private GridAdapter gridAdapter;
+
+    // * 일 저장 할 리스트
+    private ArrayList<String> dayList;
+
+    // 그리드뷰
+    private GridView gridView;
+
+    // 캘린더 변수
+    private Calendar mCal;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_fragment2, container, false);
@@ -37,55 +52,31 @@ public class Fragment2 extends Fragment {
         //연,월,일을 따로 저장
 
         final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
-
         final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
-
         final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
 
 
-
         //현재 날짜 텍스트뷰에 뿌려줌
-
         tvDate.setText(curYearFormat.format(date) + "/" + curMonthFormat.format(date));
 
-
-
         //gridview 요일 표시
-
         dayList = new ArrayList<String>();
-
         dayList.add("일");
-
         dayList.add("월");
-
         dayList.add("화");
-
         dayList.add("수");
-
         dayList.add("목");
-
         dayList.add("금");
-
         dayList.add("토");
-
-
-
         mCal = Calendar.getInstance();
 
-
-
         //이번달 1일 무슨요일인지 판단 mCal.set(Year,Month,Day)
-
         mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
-
         int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
 
         //1일 - 요일 매칭 시키기 위해 공백 add
-
         for (int i = 1; i < dayNum; i++) {
-
             dayList.add("");
-
         }
 
         setCalendarDate(mCal.get(Calendar.MONTH) + 1);
@@ -96,20 +87,6 @@ public class Fragment2 extends Fragment {
         return rootView;
     }
 
-    // 연/월 텍스트뷰
-    private TextView tvDate;
-
-    // 그리드뷰 어댑터
-    private GridAdapter gridAdapter;
-
-    // * 일 저장 할 리스트
-    private ArrayList<String> dayList;
-
-    // 그리드뷰
-    private GridView gridView;
-
-    // 캘린더 변수
-    private Calendar mCal;
 
     // 해당 월에 표시할 일 수 구함
     private void setCalendarDate(int month)
@@ -128,99 +105,59 @@ public class Fragment2 extends Fragment {
         private final List<String> list;
         private final LayoutInflater inflater;
         /**
-
          * 생성자
-
          *
-
          * @param context
-
          * @param list
-
          */
 
-        public GridAdapter(Context context, List<String> list) {
-
+        public GridAdapter(Context context, List<String> list)
+        {
             this.list = list;
-
             this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
         }
 
-
-
         @Override
-
-        public int getCount() {
-
+        public int getCount()
+        {
             return list.size();
-
         }
 
-
-
         @Override
-
-        public String getItem(int position) {
-
+        public String getItem(int position)
+        {
             return list.get(position);
-
         }
-
-
-
         @Override
 
-        public long getItemId(int position) {
-
+        public long getItemId(int position)
+        {
             return position;
-
         }
 
-
-
         @Override
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-
-
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
             ViewHolder holder = null;
-
-
-
             if (convertView == null) {
 
                 convertView = inflater.inflate(R.layout.item_calendar_gridview, parent, false);
-
                 holder = new ViewHolder();
-
-
-
                 holder.tvItemGridView = (TextView)convertView.findViewById(R.id.tv_item_gridview);
-
-
-
                 convertView.setTag(holder);
 
             } else {
 
                 holder = (ViewHolder)convertView.getTag();
-
             }
-
             holder.tvItemGridView.setText("" + getItem(position));
 
 
-
             //해당 날짜 텍스트 컬러,배경 변경
-
             mCal = Calendar.getInstance();
 
             //오늘 day 가져옴
-
             Integer today = mCal.get(Calendar.DAY_OF_MONTH);
-
             String sToday = String.valueOf(today);
 
             if (sToday.equals(getItem(position))) { //오늘 day 텍스트 컬러 변경
@@ -228,21 +165,13 @@ public class Fragment2 extends Fragment {
                 String strColor = "#color_000000";
                 // holder.tvItemGridView.setTextColor(getResources().getColor(R.color.color_000000));
                 holder.tvItemGridView.setTextColor(Color.parseColor(strColor));
-
             }
-
             return convertView;
-
         }
-
     }
 
-
-
-    private class ViewHolder {
-
+    private class ViewHolder
+    {
         TextView tvItemGridView;
-
     }
-
 }
